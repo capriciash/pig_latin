48ca2067fb5dafcf76087d1ce7284123253ad4f3
Ashley Riehl and Zachary Richards <zachary.j.richards@gmail.com>
2016-06-22 20:52:18 -0700

    got first spec to return

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
-$(document).ready(function() {
 
+var vowels = ["a", "e", "i", "o", "u"];
+var piglatin = "ay";
+
+
+
+// if input.includes(vowels) {
+//   alert("contains vowel!");
+// }
+
+$(document).ready(function() {
+  $("form#blank").submit(function(event) {
+    event.preventDefault();
+
+    var input = $("input#phrase").val();
+
+    var firstLetter = input.charAt(0);
+    var result = input.concat(piglatin);
+    
+    if (vowels.includes(firstLetter) === true) {
+      console.log(result);
+    } else {
+      console.log("false");
+    };
+
+    // console.log(firstLetter);
+    // for (var index = 0; index < vowels.length; index += 1) {
+    //   if (vowel[index] === onevowel) {
+    //     console.log("true");
+    //   } else {
+    //     console.log("false");
+    //   }
+    // };
+
+    });
 
 });
+
+//contains to find out if included in array anywhere
+
+// var first = input.charAt(0);
+// var second = input.charAt(1);
